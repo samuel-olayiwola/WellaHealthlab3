9ce11fde0bc44119f71bd5c169b8d9d560f426de
Samuel Peter <you@example.com>
2022-04-25 10:13:09 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;  
+  public class PrimeNumber  
+   {  
+     public static void Main(string[] args)  
+      {  
+
+          bool isprime = true; 
+          Console.Write("Enter the Number to check Prime: ");    
+          int number = int.Parse(Console.ReadLine());    
+          for(int i = 2; i <= number/2; i++)    
+          {    
+           if(number % i == 0)    
+            {    
+             Console.Write("Number is not Prime.");   
+             isprime = false;    
+             break;    
+            }    
+          }    
+          if (isprime)    
+           Console.Write("Number is Prime.");       
+     }  
+   }  
